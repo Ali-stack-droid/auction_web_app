1d4af81bcda527626a4d4f109d26f8650db66b70
Hassaan Asim <133565146+git-hassaan@users.noreply.github.com>
2024-12-27 01:02:30 +0500

    live streaming detail page completed

---
+++ src/components/auction/auction-components/details-components/LiveStreamingDetails.tsx
@@ -0,0 +1,40 @@
+import { Box, Typography } from '@mui/material';
+import PlaceIcon from '@mui/icons-material/Place';
+import WatchLaterRoundedIcon from '@mui/icons-material/WatchLaterRounded';
+import ViewInArRoundedIcon from '@mui/icons-material/ViewInArRounded';
+import { useAuctionDetailStyles } from '../AuctionStyles';
+import CategoryRoundedIcon from '@mui/icons-material/CategoryRounded';
+import DeviceHubRoundedIcon from '@mui/icons-material/DeviceHubRounded';
+
+const LiveStreamingDetails = ({ streamData }: any) => {
+    const classes = useAuctionDetailStyles();
+
+    return (
+        <Box className={classes.containerLive}>
+
+            {/* Lots Available */}
+            <Box className={classes.iconText}>
+                <ViewInArRoundedIcon fontSize="small" color="primary" />
+                <Typography className={classes.textLive}>{streamData.lotsAvailable} Lots Available</Typography>
+            </Box>
+
+            {/* Location */}
+            <Box className={classes.iconText}>
+                <CategoryRoundedIcon fontSize="small" color="primary" />
+                <Typography className={classes.textLive}>Categories</Typography>
+            </Box>
+
+            {/* Date Range */}
+            <Box className={`${classes.iconText} ${classes.flexItem}`}>
+                <DeviceHubRoundedIcon fontSize="small" color="primary" />
+                <Typography className={classes.textLive} sx={{ whiteSpace: 'normal', wordBreak: 'break-word' }}>
+                    Sub-Categories
+                </Typography>
+            </Box>
+
+
+        </Box>
+    );
+};
+
+export default LiveStreamingDetails;
