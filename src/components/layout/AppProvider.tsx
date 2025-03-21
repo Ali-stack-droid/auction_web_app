cbfa4ae944e521526fa7a8189cde71f27abf0d76
Hassaan Asim <133565146+git-hassaan@users.noreply.github.com>
2025-03-21 20:50:29 +0500

    ui finalized

--- src/components/layout/AppProvider.tsx
+++ src/components/layout/AppProvider.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Divider } from '@mui/material';
+import { Box, Divider, StyledEngineProvider } from '@mui/material';
 import SideBar from './sidebar/SideBar';
 import Header from './header/Header'; // Import your Header component
 
@@ -7,22 +7,24 @@ const AppProvider = ({ children }: any) => {
     const [searchTerm, setSearchTerm] = useState("");
 
     return (
-        <Box sx={{ display: 'flex', height: '100vh' }}>
-            {/* Sidebar on the left */}
-            <SideBar />
+        <StyledEngineProvider injectFirst>
+            <Box sx={{ display: 'flex', height: '100vh' }}>
+                {/* Sidebar on the left */}
+                <SideBar />
 
-            {/* Main content area */}
-            <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
-                {/* Header on top */}
-                <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-                <Divider variant='fullWidth' sx={{ opacity: 0.5 }} />
+                {/* Main content area */}
+                <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+                    {/* Header on top */}
+                    <Header searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+                    <Divider variant='fullWidth' sx={{ opacity: 0.5 }} />
 
-                {/* Content area below the header */}
-                <Box id="childContainer" sx={{ flex: 1, overflowY: 'auto', padding: 1 }}>
-                    {React.cloneElement(children, { searchTerm })}
+                    {/* Content area below the header */}
+                    <Box id="childContainer" sx={{ flex: 1, overflowY: 'auto', padding: 1 }}>
+                        {React.cloneElement(children, { searchTerm })}
+                    </Box>
                 </Box>
             </Box>
-        </Box>
+        </StyledEngineProvider>
     );
 };
 
